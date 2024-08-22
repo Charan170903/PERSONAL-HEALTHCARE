de827095d8a55900446043614a6a874dcd7756a3
Charankarthikeyan <charankarthikeyan7@gmail.com>
2024-08-22 12:34:33 +0530

    UPD

--- app/api/getResponse/route.ts
+++ app/api/getResponse/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyCoe4Qobhrm5QyCquCx1FfswrW-R_YITE4");
 
 export async function POST(request: Request) {
   const data = await request.json();
